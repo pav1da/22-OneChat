deeb8e4ea400f59626aa1e58ff03e1dfb689fc1c
Jaogaon <jaybeabxx@gmail.com>
2025-10-25 21:45:17 +0700

    Setup Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import SignIn from './page/SignIn'
+
+function App() {
+
+  return (
+    <>
+      <SignIn />
+    </>
+  )
+}
+
+export default App
---
+++ src/page/SignIn.jsx
@@ -0,0 +1,11 @@
+const SignIn = () => {
+  return (
+    <>
+      <div>
+        <h2>SIGN IN</h2>
+      </div>
+    </>
+  );
+};
+
+export default SignIn;
