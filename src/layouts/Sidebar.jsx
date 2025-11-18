a2bddb849a83f1ba9517be35ccb292944dba3ea4
miotar4 <sudarat.kre@spumail.net>
2025-11-19 06:45:09 +0700

    commit

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -2,20 +2,31 @@ import { Nav } from "react-bootstrap";
 import { useState } from "react";
 import "./Sidebar.css";
 
-const Sidebar = ({ collapsed, toggleSidebar }) => {
+const Sidebar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleSidebar = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
-    <div className={`sidebar-container p-3 position-relative ${collapsed ? "collapsed" : ""}`}>
-      
-      {/* ปุ่ม Toggle */}
-      <div className="toggle-btn" onClick={toggleSidebar}>
-        {collapsed ? ">" : "<"}
-      </div>
+    <div
+      className={
+        "sidebar-container d-flex flex-column justify-content-between p-3  " +
+        (collapsed ? "collapsed" : "")
+      }
+    >
 
       {/* Top Section */}
-      <div>
-        <div className="d-flex align-items-center mb-5">
+      <div className="top-section">
+        <div className="d-flex align-items-center mb-3">
           <div className="logo-box me-2"></div>
           <span className="logo-text">ONE CHAT</span>
+        </div>
+
+        {/* ปุ่มกดย่อ/ขยาย*/}
+        <div className="toggle-btn mt-2 mb-4 " onClick={toggleSidebar}>
+          {collapsed ? ">" : "<"}
         </div>
 
         <Nav className="flex-column">
@@ -23,7 +34,6 @@ const Sidebar = ({ collapsed, toggleSidebar }) => {
             <i className="bi bi-columns-gap"></i>
             <span className="menu-text ms-2">Dashboard</span>
           </Nav.Link>
-
           <Nav.Link href="#inbox" className="menu-item d-flex align-items-center">
             <i className="bi bi-inbox"></i>
             <span className="menu-text ms-2">Inbox</span>
@@ -35,7 +45,6 @@ const Sidebar = ({ collapsed, toggleSidebar }) => {
       <div className="bottom-section">
         <div className="profile d-flex align-items-center mb-3">
           <div className="profile-pic me-2"></div>
-
           <div className="profile-info">
             <div className="fw-bold">pav1da</div>
             <div className="small text-muted">admin</div>
@@ -47,14 +56,12 @@ const Sidebar = ({ collapsed, toggleSidebar }) => {
             <i className="bi bi-gear"></i>
             <span className="menu-text ms-2">Setting</span>
           </Nav.Link>
-
           <Nav.Link href="#logout" className="menu-item d-flex align-items-center">
             <i className="bi bi-box-arrow-right"></i>
             <span className="menu-text ms-2">Log out</span>
           </Nav.Link>
         </Nav>
       </div>
-
     </div>
   );
 };
