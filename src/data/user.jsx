a717af41650f35b88880a1b14f18e0258442274a
doraemon-kub <natthawut.sag@spumail.net>
2025-11-21 19:14:20 +0700

    mock user

---
+++ src/data/user.jsx
@@ -0,0 +1,48 @@
+export const user = [
+
+    {
+        id: 1,
+        name: "IT",
+        role: "it",
+        color: "#9C27B0",
+        email: "it@onechat.com",
+        phone: "089-999-9999"
+    },
+
+    {
+        id: 2,
+        name: "Admin A",
+        role: "admin",
+        color: "#FF0000",
+        email: "admin@onechat.com",
+        phone: "081-111-1111"
+    },
+
+
+    {
+        id: 3,
+        name: "User A",
+        role: "user",
+        color: "#2196F3",
+        email: "A@gmail.com",
+        phone: "082-222-2222"
+    },
+    {
+        id: 4,
+        name: "User B",
+        role: "user",
+        color: "#4CAF50",
+        email: "B@gmail.com",
+        phone: "083-333-3333"
+    },
+
+
+    {
+        id: 5,
+        name: "Customer VIP",
+        role: "customer",
+        color: "#FF9800",
+        email: "customer@yahoo.com",
+        phone: "085-555-5555"
+    }
+];
